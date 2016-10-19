0456cd2846190128eebd18e5a0f4b12dd1bef3bb
zaven.chilingaryan <zaven.chilingaryan@arm.synisys.com>
2016-10-19 13:17:07 +0400

    initial commit

---
+++ src/main/java/com/synisys/go/dom/User.java
@@ -0,0 +1,72 @@
+package com.synisys.go.dom;
+
+import java.util.Date;
+
+/**
+ * Created by zaven.chilingaryan on 10/18/2016.
+ */
+public class User {
+    private String name;
+    private String lastName;
+    private Integer age;
+    private String phone;
+    private String login;
+    private String password;
+    private Date birthDate = new Date();
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", lastName='" + lastName + '\'' + age +
+                '}';
+    }
+}
---
+++ src/main/java/com/synisys/go/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Created by zaven.chilingaryan on 10/18/2016.
+ */
+package com.synisys.go;
